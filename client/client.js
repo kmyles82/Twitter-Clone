d1442ff110d860ff75fb1afa0f4c71bbe653db09
kmyles82 <kmyles82@gmail.com>
2019-08-06 14:51:03 -0500

    added functions to get all meows from db and display on web page

--- client/client.js
+++ client/client.js
@@ -1,8 +1,11 @@
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const meowsElement = document.querySelector('.meows');
 const API_URL = 'http://localhost:5000/meows';
 
-loadingElement.style.display = 'none';
+loadingElement.style.display = '';
+
+listAllMeows();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -15,9 +18,10 @@ form.addEventListener('submit', (event) => {
         name,
         content
     }
-    loadingElement.style.display = 'block';
+    loadingElement.style.display = '';
     form.style.display = 'none';
 
+    //sending data to backend
     fetch(API_URL, {
         method: 'POST',
         body: JSON.stringify(meow),
@@ -25,4 +29,36 @@ form.addEventListener('submit', (event) => {
             'content-type': 'application/json'
         }
     })
+    //get back response from backend in json format
+    .then(response => response.json())
+    //get back access to createdMeow
+    .then(createdMeow => {
+        console.log(createdMeow)
+        form.reset();
+        loadingElement.style.display = 'none';
+        form.style.display = '';
+    })
 })
+
+function listAllMeows(){
+    fetch(API_URL)
+        //get back response from backend in json format
+        .then(response => response.json())
+        //get back access to createdMeow
+        .then(meows => {
+            meows.forEach(meow => {
+                const div = document.createElement('div');
+                
+                const header = document.createElement('h3');
+                header.textContent = meow.name;
+                
+                const contents = document.createElement('p');
+                contents.textContent = meow.content;
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                meowsElement.appendChild(div);
+            })
+            console.log(meows)
+    })
+}
